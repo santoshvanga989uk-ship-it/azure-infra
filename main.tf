35eb37ada55ef2819716c1f0ff4c79efa61ad373
santosh vanga <santoshvanga989uk@gmail.com>
2026-03-17 23:09:50 +0000

    Rename storage account in main.tf

--- main.tf
+++ main.tf
@@ -8,7 +8,7 @@ resource "azurerm_resource_group" "example" {
 }
 
 resource "azurerm_storage_account" "example" {
-  name                     = "storageaccountname-santoshdemo9948"
+  name                     = "storageaccountname-sansantosh9948"
   resource_group_name      = azurerm_resource_group.example.name
   location                 = azurerm_resource_group.example.location
   account_tier             = "Standard"
