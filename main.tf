63e92d3f6d69d6810f22ade4352208556a33a1b8
santosh vanga <santoshvanga989uk@gmail.com>
2026-03-17 23:02:29 +0000

    Update storage account name to lowercase

--- main.tf
+++ main.tf
@@ -8,7 +8,7 @@ resource "azurerm_resource_group" "example" {
 }
 
 resource "azurerm_storage_account" "example" {
-  name                     = "StorageAccountName-santoshdemo-9948"
+  name                     = "storageaccountname-santoshdemo-9948"
   resource_group_name      = azurerm_resource_group.example.name
   location                 = azurerm_resource_group.example.location
   account_tier             = "Standard"
